21e7746aa1f469099c2c50d9eaf1c6766fa75a5a
Paulo Machado <PauloMMachado2003@gmail.com>
2022-11-10 02:17:46 -0300

    i didn't keep track of it

--- script.js
+++ script.js
@@ -1,26 +1,45 @@
 let grid = document.querySelector('#grid');
+let button = document.querySelector('button');
 
-//generating grid
-for(let i = 0; i < 16; i++) {
-    let row = document.createElement('div');
-    row.setAttribute('class', 'row');
-    row.setAttribute('display', 'flex');
-    
-    for (let o = 0; o < 16; o++) {
-        let component = document.createElement('div');
-        component.setAttribute('class', 'component');
-        //component.textContent = `${o}`;
-
-        row.appendChild(component);
-    }
-
-    grid.appendChild(row);
-}
+button.addEventListener('click', () => {
+    let gSize = prompt('informe o tamanho dos lados da grade', 16);
+    let body = document.querySelector('body');
+    body.removeChild(grid);
+    grid = document.createElement('div');
+    grid.setAttribute('id', 'grid');
+    body.appendChild(grid);
+    generateGrid(gSize);
+});
 
 function addHover(component) {
     component.classList.toggle('onhover');
 }
 
-let components = document.querySelectorAll('.component');
-components.forEach( component => component.addEventListener('mouseover', () => addHover(component)))
-components.forEach( component => component.addEventListener('mouseout', () => addHover(component)))
+function generateGrid(gSize) {
+    for(let i = 0; i < gSize; i++) {
+        let row = document.createElement('div');
+        row.setAttribute('class', 'row');
+        row.setAttribute('display', 'flex');
+        let squareSize = 500/gSize;
+        console.log(squareSize);
+        
+        for (let o = 0; o < gSize; o++) {
+            let component = document.createElement('div');
+            component.setAttribute('class', 'component');
+            component.style.cssText = `padding: ${squareSize}px;`;
+    
+            row.appendChild(component);
+        }
+    
+        grid.appendChild(row);
+    }
+
+    let components = document.querySelectorAll('.component');
+    components.forEach( component => component.addEventListener('mouseover', () => addHover(component)))
+}
+
+generateGrid(16)
+
+
+
+
